d9df83efef3a2de9874fb5c513f03eef6ab50f92
JacobAhlberg <jacahlberg@hotmail.com>
2017-02-19 14:23:33 +0100

    Declaring buttons + design

--- Acalculator/app/src/main/java/com/example/jacob/acalculator/MainActivity.java
+++ Acalculator/app/src/main/java/com/example/jacob/acalculator/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.jacob.acalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0,
+            addBtn, subtractBtn, equalBtn, divdeBtn, timeBtn;
+    TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialising buttons and textview
+        resultView = (TextView)findViewById(R.id.resultView);
+        btn0 = (Button)findViewById(R.id.btn0);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+        btn5 = (Button)findViewById(R.id.btn5);
+        btn6 = (Button)findViewById(R.id.btn6);
+        btn7 = (Button)findViewById(R.id.btn7);
+        btn8 = (Button)findViewById(R.id.btn8);
+        btn9 = (Button)findViewById(R.id.btn9);
+
+
+        
+
+
+
     }
 }
